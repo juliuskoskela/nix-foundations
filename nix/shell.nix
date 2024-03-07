c23479aef997130b1e24ecb8b55f452210f75a3f
Julius Koskela <julius.koskela@unikie.com>
2024-03-07 14:27:18 +0200

    Add shell.nix
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- nix/shell.nix
+++ nix/shell.nix
@@ -7,7 +7,6 @@ let
   aliases = [
     { alias = "md-watch"; command = "marp -w src/markdown"; }
     { alias = "md-edit"; command = "marktext src/markdown &"; }
-    { alias = "md-preview"; command = "md-preview firefox src/markdown/*.html"; }
   ];
 
   # Adjust the transformation function to return the correct structure.
@@ -21,9 +20,6 @@ pkgs.mkShell {
     marp-cli
     firefox
     marktext
-	# md-watch
-	# md-edit
-	# md-preview
   ] ++ mapAliases;
 
   shellHook = ''
@@ -31,7 +27,6 @@ pkgs.mkShell {
     Commands:
 	  md-watch: Watch markdown files and generate HTML
 	  md-edit: Open markdown files in Mark Text
-	  md-preview: Open generated HTML files in Firefox
 	  "
   '';
 }
